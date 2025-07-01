30d48675532f1a9977915b4dc38f02a0858e1307
Efnan <genc.efnan4@gmail.com>
2025-07-01 19:06:26 +0300

    postgreSql connected

--- user_project/user_project/settings.py
+++ user_project/user_project/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'user_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'userDb',  
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
